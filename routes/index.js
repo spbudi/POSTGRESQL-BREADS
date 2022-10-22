06c4e4457652e5989e06a3fd3205b66132112a29
spbudi <setyopambudi34@gmail.com>
2022-10-22 14:25:13 +0700

    pagination

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,39 @@ var express = require('express');
 var router = express.Router();
 const moment = require('moment');
 
-/* GET home page. */
 module.exports = function (db) {
-  router.get('/', function (req, res, next) {
-    db.query('SELECT * FROM todos', (err, data) => {
+
+// HOME
+  router.get('/', function (req, res) {
+    let page = req.query.page || 1;
+    page = Number(page)
+    const limit = 3;
+    const offset = page * limit - limit ; 
+
+    db.query('SELECT COUNT(*) as total FROM todos', (err, data) => {
       if (err) return res.send(err);
-      res.render('list', { todos: data.rows, moment });
+      const total = data.rows[0].total;
+      const pages = Math.ceil(total / limit);
+
+      db.query('SELECT * FROM todos LIMIT $1 OFFSET $2',[limit, offset],(err, data) => {
+          if (err) return res.send(err);
+          res.render('list', {
+            todos: data.rows,
+            moment,
+            page,
+            pages,
+            offset
+          })
+        }); 
     });
   });
 
-  router.get('/add', function (req, res, next) {
+// ADD
+  router.get('/add', function (req, res) {
     res.render('add', {});
   });
 
-  router.post('/add', function (req, res, next) {
+  router.post('/add', function (req, res) {
     const { string, integer, float, date, boolean } = req.body;
     db.query(
       'INSERT INTO todos(string, integer, float, date, boolean) values($1, $2, $3, $4, $5)',
@@ -27,17 +46,24 @@ module.exports = function (db) {
     );
   });
 
-  router.get('/edit/:id', function (req, res, next) {
-    db.query('SELECT * FROM todos WHERE id = $1', [Number(req.params.id)],(err, data) => {
-      if (err) return res.send(err);
-      if(data.rows.length == 0) res.send('data not found')
-      res.render('edit', {item: data.rows[0], moment});
-    });
+// EDIT
+  router.get('/edit/:id', function (req, res) {
+    db.query(
+      'SELECT * FROM todos WHERE id = $1',
+      [Number(req.params.id)],
+      (err, data) => {
+        if (err) return res.send(err);
+        if (data.rows.length == 0) res.send('data not found');
+        res.render('edit', { item: data.rows[0], moment });
+      }
+    );
   });
 
-  router.post('/edit/:id', function (req, res, next) {
+  router.post('/edit/:id', function (req, res) {
     const { string, integer, float, date, boolean } = req.body;
-    db.query('UPDATE todos SET string = $1 , integer = $2, float = $3, date = $4, boolean = $5 WHERE id = $6', [string, integer, float, date, boolean, Number(req.params.id)],
+    db.query(
+      'UPDATE todos SET string = $1 , integer = $2, float = $3, date = $4, boolean = $5 WHERE id = $6',
+      [string, integer, float, date, boolean, Number(req.params.id)],
       (err) => {
         if (err) return res.send(err);
         res.redirect('/');
@@ -45,11 +71,15 @@ module.exports = function (db) {
     );
   });
 
-  router.get('/delete/:id', function (req, res, next) {
-    db.query('DELETE FROM todos WHERE id = $1', [Number(req.params.id)],(err, data) => {
-      if (err) return res.send(err);
-      res.redirect('/')
-    });
+// DELETE
+  router.get('/delete/:id', function (req, res) {
+    db.query(
+      'DELETE FROM todos WHERE id = $1',
+      [Number(req.params.id)],
+      (err, data) => {
+        if (err) return res.send(err);
+        res.redirect('/');
+      });
   });
 
   return router;
