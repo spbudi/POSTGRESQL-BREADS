485e2b2f0176b787a22460781d570ee00e95bfec
spbudi <setyopambudi34@gmail.com>
2022-10-23 00:41:46 +0700

    filters

--- routes/index.js
+++ routes/index.js
@@ -3,33 +3,94 @@ var router = express.Router();
 const moment = require('moment');
 
 module.exports = function (db) {
-
-// HOME
+  // HOME
   router.get('/', function (req, res) {
-    let page = req.query.page || 1;
-    page = Number(page)
-    const limit = 3;
-    const offset = page * limit - limit ; 
+    // console.log(req.url);
+    const url = req.url == '/' ? '/?page=1' : req.url
+    console.log(url);
 
-    db.query('SELECT COUNT(*) as total FROM todos', (err, data) => {
+    // SEARCHING
+    let params = [];
+    let counter = 1;
+    let values = []
+
+    if(req.query.id && req.query.idCheck == 'on'){
+      values.push(req.query.id)
+      params.push(`id = $${values.length}`)
+    }
+
+    if(req.query.string && req.query.stringCheck == 'on'){
+      values.push(req.query.string)
+      params.push(`string ilike '%' || $${values.length} || '%'`)
+    }
+
+    if(req.query.integer && req.query.integerCheck == 'on'){
+      values.push(req.query.integer)
+      params.push(`integer = $${values.length}`)
+    }
+
+    if(req.query.float && req.query.floatCheck == 'on'){
+      values.push(req.query.float)
+      params.push(`float = $${values.length}`)
+    }
+
+    if(req.query.fromdate && req.query.todate){
+      params.push(`date BETWEEN $${values.length + 1} AND $${values.length + 2}`)
+      values.push(req.query.fromdate)
+      values.push(req.query.todate)
+    } else if(req.query.fromdate){
+      values.push(req.query.fromdate)
+      params.push(`date >= $${values.length}`)
+    } else if(req.query.todate){
+      values.push(req.query.todate)
+      params.push(`date <= $${values.length}`)
+    }
+
+    if(req.query.boolean && req.query.booleanCheck == 'on'){
+      values.push(req.query.boolean)
+      params.push(`boolean = $${values.length}`)
+    }
+
+    let page = req.query.page || 1;
+    page = Number(page);
+    const limit = 3;
+    const offset = page * limit - limit;
+
+    let sql = 'SELECT COUNT(*) as total FROM todos'
+    if(params.length > 0){
+      sql += ` WHERE ${params.join(' AND ')}`
+    }
+
+    db.query(sql, values,(err, data) => {
       if (err) return res.send(err);
       const total = data.rows[0].total;
       const pages = Math.ceil(total / limit);
 
-      db.query('SELECT * FROM todos LIMIT $1 OFFSET $2',[limit, offset],(err, data) => {
+      sql = 'SELECT * FROM todos'
+    if(params.length > 0){
+      sql += ` WHERE ${params.join(' AND ')}`
+    }
+    sql += ` LIMIT $${values.length + 1} OFFSET $${values.length + 2}`
+
+    console.log('sql', sql);
+
+      db.query(sql, [...values, limit, offset], (err, data) => {
           if (err) return res.send(err);
           res.render('list', {
             todos: data.rows,
             moment,
             page,
             pages,
-            offset
-          })
-        }); 
+            offset,
+            query: req.query,
+            url
+          });
+        }
+      );
     });
   });
 
-// ADD
+  // ADD
   router.get('/add', function (req, res) {
     res.render('add', {});
   });
@@ -46,7 +107,7 @@ module.exports = function (db) {
     );
   });
 
-// EDIT
+  // EDIT
   router.get('/edit/:id', function (req, res) {
     db.query(
       'SELECT * FROM todos WHERE id = $1',
@@ -71,7 +132,7 @@ module.exports = function (db) {
     );
   });
 
-// DELETE
+  // DELETE
   router.get('/delete/:id', function (req, res) {
     db.query(
       'DELETE FROM todos WHERE id = $1',
@@ -79,7 +140,8 @@ module.exports = function (db) {
       (err, data) => {
         if (err) return res.send(err);
         res.redirect('/');
-      });
+      }
+    );
   });
 
   return router;
